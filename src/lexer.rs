166bdc9979c32009d01ed7bec0180748c54b23cb
Tym Drogin <tim.drogin@gmail.com>
2023-09-23 20:53:20 +0100

    added a lot of function. Played with an option to handle invalid indexing

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,39 +1,39 @@
 use anyhow::Result;
+use std::option;
 #[derive(Debug, PartialEq)]
 enum Token {
-    EoF,          // End of input
+    EoF,                // End of file
 
     // Parentheses and Brackets
-    LParen,    // Left parenthesis
-    RParen,    // Right parenthesis
-    LSquiggly, // Left brace
-    RSquiggly, // Right brace
-    LSquare,   // Left square bracket
-    RSquare,   // Right square bracket
+    LParen,             // Left parenthesis
+    RParen,             // Right parenthesis
+    LSquiggly,          // Left brace
+    RSquiggly,          // Right brace
+    LSquare,            // Left square bracket
+    RSquare,            // Right square bracket
 
     // Literals
-    Integer(i64), // Integer literals
-    Float(f64),   // Floating-point literals
-    String(String),// String literals
-    Symbol(String),// Symbols and Identifiers
-    Keyword(String),// Keywords
+    Integer(i64),       // Integer literals
+    Float(f64),         // Floating-point literals
+    String(String),     // String literals
 
-    // Special Characters
-    Quote,         // Quote character (')
-    Dot,           // Dot (.) for cons cells
+    //Symbols, ident and keyword
+    Symbol(String),     // For any special symbol or combinations like +, -, =, >=, ~@, etc
+    Ident(String),      // Identifiers such as function names
+    Keyword(String),    // Keywords
 
     // Booleans and Nil
-    True,          // Boolean true
-    False,         // Boolean false
-    Nil,           // Nil (often used for empty lists)
+    True,               // Boolean true
+    False,              // Boolean false
+    Nil,                // Nil (often used for empty lists)
 
-    Comment(String), // Comments
+    Comment(String),    // Comments
 }
 
 #[allow(unused)]
 pub struct Lexer {
     position: usize,
-    ch: u8,
+    curr_char: Option<u8>,
     input: Vec<u8>,
 }
 
@@ -41,72 +41,64 @@ impl Lexer {
     pub fn new(input: String) -> Lexer {
         let mut lex = Lexer {
             position: 0,
-            ch: 0,
+            curr_char: None,
             input: input.into_bytes(),
         };
+        lex.read_char();
         return lex;
     }
-
-
-    fn next(&mut self) -> Result<Token> {
-        self.skip_whitespace();
-        
-        let token = match self.ch {
-            b'(' => Token::LParen,
-            b')' => Token::RParen,
-            b'{' => Token::LSquiggly,
-            b'}' => Token::RSquiggly,
-            b'[' => Token::LSquare,
-            b']' => Token::RSquare,
-
-            b';' => {
-                let mut comment = String::new();
-                loop {
-                    self.read_char();
-                    if self.peek() == b'\n' {
-                        break;
-                    }
-                    comment.push(str::from_utf8(self.ch));
-                }
-                Token::Comment(comment)
-            }
-
-
-
-
-
-
-            b'a'..=b'z' | b'A'..=b'Z' | b'_' => {}
-            b'0'..=b'9' => {}
-
-            0 => Token::EoF,
-        };
+    pub fn tokenize() -> Vec<Token> {
+        todo!();
     }
 
+    fn next_token() -> Token {
+        todo!();
+    }
+
+
+
+
+
+    fn parse_number() -> Token {
+        todo!()
+    }
+    fn parse_ident() -> Token {
+        todo!()
+    }
 
     fn read_char(&mut self) {
-        if self.position >= self.input.len() {
-            self.ch = 0;
-        } else {
-            self.ch = self.input[self.position];
-        }
+        self.curr_char = self.peek();
         self.advance();
     }
-    fn read_identifier() {
-        TODO!();
+    fn peek(&mut self) -> Option<u8> {
+        if self.position >= self.input.len() {
+            None
+        } else {
+            Some(self.input[self.position])
+        }
+    }
+    fn advance(&mut self) {
+        self.position += 1;
     }
     fn skip_whitespace(&mut self) {
-        while self.ch.is_ascii_whitespace() || self.ch == b',' {
-        self.read_char();
+        while self.curr_char.is_ascii_whitespace() || self.curr_char.unwrap() == b',' {
+
         }
     }
-    fn advance(&mut self) {self.position += 1;}
-    fn peek(&self) -> u8 {
-        if self.position < self.input.len() {
-            return self.input[self.ch];
-        } else {
-            return 0;
-        }
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
