342b9a5ad24f97df7129932204cf60e44a710417
Tym Drogin <tim.drogin@gmail.com>
2023-09-25 00:03:21 +0100

    not done but getting there

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,30 +1,65 @@
-#[derive(Debug, PartialEq)]
-#[allow(unused)]
-pub enum Token {
-    EoF,                // End of file
+use logos::{Logos, Lexer};
 
+fn to_string(lex: &mut Lexer<Token>) -> Option<String>{
+    Some(lex.slice().to_string())
+}
+
+
+#[derive(Logos, Debug, PartialEq)]
+#[logos(skip r"[\s,]")] // Ignore this regex pattern between tokens
+pub enum Token {
     // Parentheses and Brackets
+
+    #[token("(")]
     LParen,             // Left parenthesis
+    #[token(")")]
     RParen,             // Right parenthesis
+    #[token("{")]
     LSquiggly,          // Left brace
+    #[token("}")]
     RSquiggly,          // Right brace
+    #[token("[")]
     LSquare,            // Left square bracket
+    #[token("]")]
     RSquare,            // Right square bracket
 
-    // Literals
-    Integer(i64),       // Integer literals
-    Float(f64),         // Floating-point literals
+    #[regex(r"-?[0-9]*(.[0-9]+)?", to_string)]
+    Number(String),
+    #[regex(r#"(?:[^"\\]|\\.)*"#, to_string)]
     String(String),     // String literals
 
     //Symbols, ident and keyword
+    //FIXME
+    #[token("+", "-", "*", "/"
+    "'", "`", "^", "~@", "~", "@")
+    ]
     Symbol(String),     // For any special symbol or combinations like +, -, =, >=, ~@, etc
+    #[regex("[a-zA-Z_]*", to_string)]
     Ident(String),      // Identifiers such as function names
+    //FIXME
+    #[token("defun",to_string)]
     Keyword(String),    // Keywords
 
     // Booleans and Nil
+    #[token("true")]
     True,               // Boolean true
+    #[token("false")]
     False,              // Boolean false
+    #[token("nil")]
     Nil,                // Nil (often used for empty lists)
-
+    #[regex(";.*", to_string)]
     Comment(String),    // Comments
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn it_can_recognize_keywords() {
+        let mut lexer = Token::lexer("true false nil defun");
+
+        assert_eq!(lexer.next(), Some(Token::True));
+        assert_eq!(lexer.next(), Some(Token::False));
+        assert_eq!(lexer.next(), Some(Token::Nil));
+        assert_eq!(lexer.next(), Some(Token::Keyword("defun".to_owned())));
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,5 @@
 mod lexer;
+
 fn main() {
-    // `()` can be used when no completer is required
-    let input = "(+ 2 (* 3 4))";
 
-    let mut lexer = lexer::Lexer::new(input.to_string());
-    let tokens = lexer.tokenize();
-
-    for token in tokens {
-        println!("{:?}", token);
-    }
 }
-
